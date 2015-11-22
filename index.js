88361cddb94e87d5779e65288d7fe90e9e0bc7c1
qanper <mail@s3f.ru>
2015-11-22 12:24:13 +0300

    add Allpass

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  */
 import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
 import Adsr from 'stagas/adsr';
+import Allpass from 'opendsp/allpass';
 
 import Debug from 'debug';
 
@@ -12,14 +13,22 @@ function makeSynth(adsr, wf) {
   var _freq = 440;
   var synth = {};
   
+  function playFreq(freq, amp) {
+    _freq = freq;
+    _amp = amp;
+    adsr.play(0);
+  }
+  
   return {
     out: function (t) {
+      var signal = 0;
       return wf(t,_freq) * adsr(t) * _amp;
     }, 
-    play: function (t, freq, amp) {
-      _freq = freq;
-      _amp = amp;
-      adsr.play(t);
+    playFreq: function (freq, amp) {
+      playFreq(freq, amp);
+    },
+    play: function (note, velocity) {
+      playFreq(freq, velocity/128);
     }
   }
 }
@@ -28,12 +37,14 @@ var debug = Debug('test');
 
 var synth = [makeSynth(Adsr({ a: 5 }), sin),makeSynth(Adsr({ a: 1 }), saw),makeSynth(Adsr({ a: 1 }), sin)]
 
+var ap = Allpass(3000);
+
 export function dsp(t) {
   
-  if ((2*t + 0  ) % 1   === 0) synth[0].play(t, 440, 0.2);
-  if ((3*t      ) % 1   === 0) synth[1].play(t, 330, 0.2);
-  if ((3*t      ) % 1   === 0) synth[2].play(t, 110, 0.2);
+  if ((2*t + 0  ) % 1   === 0) synth[0].playFreq(440, 0.2);
+  if ((3*t      ) % 1   === 0) synth[1].playFreq(330, 0.2);
+  if ((3*t      ) % 1   === 0) synth[2].playFreq(110, 0.2);
   
 
-  return synth[0].out(t) + synth[1].out(t) + synth[2].out(t);
+  return /*synth[0].out(t) +*/ ap.run(synth[1].out(t)) /*+ synth[2].out(t)*/;
 }
