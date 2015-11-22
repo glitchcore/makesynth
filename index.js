9c6f57d9fa77c84a9e1aebf3b5e59a9670d362ee
qanper <mail@s3f.ru>
2015-11-22 12:29:17 +0300

    add module

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 
 /**
- * @name first
+ * @module makeSynth
+ * @author qanper
+ * @org qanper
+ * @desc synth factory
+ * @license gpl3
  */
-import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
+ 
+
+
 import Adsr from 'stagas/adsr';
 import Allpass from 'opendsp/allpass';
 
 import Debug from 'debug';
 
-function makeSynth(adsr, wf) {
+export default Synth;
+
+function makeSynth(adsrParam, wf) {
   var _amp = 0;
   var _freq = 440;
   var synth = {};
+  var _adsr = Adsr(adsrParam);
   
   function playFreq(freq, amp) {
     _freq = freq;
     _amp = amp;
-    adsr.play(0);
+    _adsr.play(0);
   }
   
   return {
     out: function (t) {
       var signal = 0;
-      return wf(t,_freq) * adsr(t) * _amp;
+      return wf(t,_freq) * _adsr(t) * _amp;
     }, 
     playFreq: function (freq, amp) {
       playFreq(freq, amp);
@@ -33,18 +42,4 @@ function makeSynth(adsr, wf) {
   }
 }
 
-var debug = Debug('test');
 
-var synth = [makeSynth(Adsr({ a: 5 }), sin),makeSynth(Adsr({ a: 1 }), saw),makeSynth(Adsr({ a: 1 }), sin)]
-
-var ap = Allpass(3000);
-
-export function dsp(t) {
-  
-  if ((2*t + 0  ) % 1   === 0) synth[0].playFreq(440, 0.2);
-  if ((3*t      ) % 1   === 0) synth[1].playFreq(330, 0.2);
-  if ((3*t      ) % 1   === 0) synth[2].playFreq(110, 0.2);
-  
-
-  return /*synth[0].out(t) +*/ ap.run(synth[1].out(t)) /*+ synth[2].out(t)*/;
-}
--- test.js
+++ test.js
@@ -3,3 +3,20 @@
  * test
  */
 
+import { sin, saw, ramp, tri, sqr, pulse, noise } from 'opendsp/osc';
+
+var debug = Debug('test');
+
+var synth = [makeSynth({ a: 5 }, sin),makeSynth({ a: 1 }, saw),makeSynth({ a: 1 }, sin)]
+
+var ap = Allpass(3000);
+
+export function dsp(t) {
+  
+  if ((2*t + 0  ) % 1   === 0) synth[0].playFreq(440, 0.2);
+  if ((3*t      ) % 1   === 0) synth[1].playFreq(330, 0.2);
+  if ((3*t      ) % 1   === 0) synth[2].playFreq(110, 0.2);
+  
+
+  return /*synth[0].out(t) +*/ ap.run(synth[1].out(t)) /*+ synth[2].out(t)*/;
+}
